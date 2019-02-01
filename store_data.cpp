15ea2599c0b986a3acc8ad13d03b055f9a0e3e7a
Maurizio <zeta_persei@hotmail.com>
2019-02-01 14:15:58 +0100

    Update and rename store_data.c to store_data.cpp

---
+++ store_data.cpp
@@ -0,0 +1,55 @@
+/* 
+*************************************************************************
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+   PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT 
+   HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+   ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+   OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF THE COPYRIGHT HOLDERS OR 
+   CONTRIBUTORS ARE AWARE OF THE POSSIBILITY OF SUCH DAMAGE.
+************************************************************************
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+     
+int main (void) {
+
+  // Variable declaration  
+ char device[] = "28-xxxx";      // Dev ID
+ char devPath[128]; // Path to device
+ char buf[256];     // Data from device
+ char tmpData[6];   // Temp C * 1000 reported by device 
+ char path[] = "/sys/bus/w1/devices"; 
+ ssize_t numRead;
+ 
+     //  code for DS18B20 sensor 1-wire acquisition 
+     
+  sprintf(devPath, "%s/%s/w1_slave", path, device);
+  int fd = open(devPath, O_RDONLY);
+  if(fd == -1)
+  {
+   perror ("Couldn't open the w1 device.");
+   return 1;   
+  }
+  while((numRead = read(fd, buf, 256)) > 0) 
+  {
+   strncpy(tmpData, strstr(buf, "t=") + 2, 5); 
+   float tempC = strtof(tmpData, NULL);
+   printf("Device: %s  - ", dev); 
+   printf("Temp: %.3f C  ", tempC / 1000);
+   printf("%.3f F\n\n", (tempC / 1000) * 9 / 5 + 32);
+  }
+  
+  close(fd);
+ 
+        
+}
